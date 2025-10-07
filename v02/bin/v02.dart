30b874b5572b0005fe642ca847b07548ac428279
Cnasboll <52800122+Cnasboll@users.noreply.github.com>
2025-10-07 08:54:17 +0200

    V02 (#1)
    v02

---
+++ v02/bin/v02.dart
@@ -0,0 +1,345 @@
+import 'dart:io';
+
+import 'package:v02/models/hero.dart';
+import 'package:v02/persistence/hero_repository.dart';
+import 'package:v02/utils/enum_parsing.dart';
+
+Future<void> main() async {
+  print("Welcome to the Hero Manager!");
+  var repo = HeroRepository('v02.db');
+
+  var doWOrk = true;
+  Map<String, (Function, String)> commands = {
+    "a": (() => createHero(repo), "[A]dd a new hero (will prompt for details)"),
+    "l": (() => listHeroes(repo), "[L]ist all heroes"),
+    "t": (
+      () => listTopNHeroes(repo),
+      "List [T]op n heroes (will prompt for n)",
+    ),
+    "s": (
+      () => listMatchingHeroes(repo),
+      "[S]earch matching heroes (will prompt for a search string)",
+    ),
+    "u": (() => updateHero(repo), "[U]pdate a hero"),
+    "d": (() => deleteHero(repo), "[D]elete a hero"),
+    "c": (() => deleteAllHeroes(repo), "[C]lean database (delete all heroes)"),
+    "q": (
+      () => {
+        if (promptQuit()) {doWOrk = false},
+      },
+      "[Q]uit (exit the program)",
+    ),
+  };
+
+  var prompt = generatePrompt(commands);
+
+  while (doWOrk) {
+    print(prompt);
+    try {
+      await mainMenu(repo, commands);
+    } catch (e) {
+      print("Unexpected error: $e");
+    }
+
+    // allow any pending async operations to complete to save changes
+    await Future.delayed(Duration.zero);
+  }
+}
+
+String generatePrompt(Map<String, (Function, String)> commands) {
+  StringBuffer promptBuffer = StringBuffer();
+  promptBuffer.write("""
+Enter a menu option (""");
+  for (int i = 0; i < commands.length; i++) {
+    if (i > 0) {
+      if (i == commands.length - 1) {
+        promptBuffer.write(" or ");
+      } else {
+        promptBuffer.write(", ");
+      }
+    }
+    promptBuffer.write(commands.keys.elementAt(i).toUpperCase());
+  }
+  promptBuffer.writeln(") and press enter:");
+
+  for (var entry in commands.entries) {
+    promptBuffer.writeln(entry.value.$2);
+  }
+  return promptBuffer.toString();
+}
+
+Future<void> mainMenu(
+  HeroRepository repo,
+  Map<String, (Function, String)> commands,
+) async {
+  var input = (stdin.readLineSync() ?? "").toLowerCase().trim();
+  if (input.isEmpty)
+  {
+    print("Please enter a command");
+    return;
+  }
+  var command = commands[input.substring(0, 1)]?.$1;
+  if (command == null) {
+    print("Invalid command, please try again");
+    return;
+  }
+  command();
+}
+
+bool promptQuit() {
+  if (promptForYesNo("Do you really want to exit?") == YesNo.no) {
+    return false;
+  }
+  print("Exiting...");
+  return true;
+}
+
+void listHeroes(HeroRepository repo) {
+  var heroes = repo.heroes;
+  if (heroes.isEmpty) {
+    print("No heroes found");
+  } else {
+    print("Found ${heroes.length} heroes:");
+    for (var hero in heroes) {
+      print(hero);
+    }
+  }
+}
+
+void listTopNHeroes(HeroRepository repo) {
+  print("Enter number of heroes to list:");
+  var input = (stdin.readLineSync() ?? "").trim();
+  var n = int.tryParse(input) ?? 0;
+  if (n <= 0) {
+    print("Invalid number");
+    return;
+  }
+  var snapshot = repo.heroes;
+  for (int i = 0; i < n; i++) {
+    if (i >= snapshot.length) {
+      break;
+    }
+    print(snapshot[i]);
+  }
+}
+
+void listMatchingHeroes(HeroRepository repo) {
+  var result = search(repo);
+  if (result == null) {
+    return;
+  }
+  for (var hero in result) {
+    print(hero);
+  }
+}
+
+void deleteAllHeroes(HeroRepository repo) {
+  if (promptForYesNo("Do you really want to delete all heroes?") == YesNo.no) {
+    return;
+  }
+  repo.clean();
+  print("Deleted all heroes");
+}
+
+enum YesNo { yes, no }
+
+YesNo promptForYesNo(String prompt) {
+  for (;;) {
+    print('''
+
+$prompt (y/n)''');
+    var input = (stdin.readLineSync() ?? "").trim().toLowerCase();
+    if (input.startsWith("y")) {
+      return YesNo.yes;
+    }
+    if (input.startsWith("n")) {
+      return YesNo.no;
+    }
+    print("Invalid answer, please enter y or n");
+  }
+}
+
+enum YesNoCancel { yes, no, cancel }
+
+YesNoCancel promptForYesNextCancel(String prompt) {
+  for (;;) {
+    print("$prompt (y = yes, n = next hero, c = cancel)");
+    var input = (stdin.readLineSync() ?? "").trim().toLowerCase();
+    if (input.startsWith("y")) {
+      return YesNoCancel.yes;
+    }
+    if (input.startsWith("n")) {
+      return YesNoCancel.no;
+    }
+    if (input.startsWith("c")) {
+      return YesNoCancel.cancel;
+    }
+    print("Invalid answer, please enter y, n or c");
+  }
+}
+
+void deleteHero(HeroRepository repo) {
+  Hero? hero = query(repo, "Delete");
+  if (hero == null) {
+    return;
+  }
+
+  if (promptForYesNo(
+        '''Do you really want to delete hero with the following details?$hero''',
+      ) ==
+      YesNo.no) {
+    return;
+  }
+
+  repo.delete(hero);
+  print('''Deleted hero:
+$hero''');
+}
+
+void createHero(HeroRepository repo) {
+  Hero? hero = promptForNew();
+  if (hero == null) {
+    print("Aborted");
+    return;
+  }
+
+  if (promptForYesNo('''Save new hero with the following details?$hero''') ==
+      YesNo.no) {
+    return;
+  }
+
+  repo.persist(hero);
+  print('''Created hero:
+$hero''');
+}
+
+void updateHero(HeroRepository repo) {
+  Hero? hero = query(repo, "Update");
+  if (hero == null) {
+    return;
+  }
+  var updatedHero = promptForUpdated(hero);
+  if (updatedHero != null) {
+    repo.persist(updatedHero);
+    print('''Updated hero:
+$updatedHero''');
+  }
+}
+
+List<Hero>? search(HeroRepository repo) {
+  print("Enter a search string:");
+  var query = (stdin.readLineSync() ?? "").trim();
+  var results = repo.query(query);
+  if (results.isEmpty) {
+    print("No heroes found");
+    return null;
+  }
+  print("Found ${results.length} heroes:");
+  return results;
+}
+
+Hero? query(HeroRepository repo, String what) {
+  var results = search(repo);
+  if (results == null) {
+    return null;
+  }
+  for (var hero in results) {
+    switch (promptForYesNextCancel('''
+
+$what the following hero?$hero''')) {
+      case YesNoCancel.yes:
+        return hero;
+      case YesNoCancel.no:
+        continue;
+      case YesNoCancel.cancel:
+        return null;
+    }
+  }
+  return null;
+}
+
+List<String> promptForUpdate(List<(String, String, String)> fieldsHintsAndCurrent) {
+  List<String> values = [];
+  for (var (field, hint, current) in fieldsHintsAndCurrent) {
+    print("Enter $field ($hint) or enter to keep current value ($current):");
+    var input = (stdin.readLineSync() ?? "").trim();
+    if (input.isEmpty) {
+      values.add(current);
+    } else {
+      values.add(input);
+    }
+  }
+  return values;
+}
+
+Hero? promptForUpdated(Hero hero) {
+  List<(String, String, String)> fieldsHintsAndCurrent = [];
+  for (int i = 1; i < Hero.fields.length; i++) {
+    var field = Hero.fields[i];
+    var hint = Hero.hints[i];
+    var value = hero.stringProps[i];
+    fieldsHintsAndCurrent.add((field, hint, value.toString()));
+  }
+
+  var update = promptForUpdate(fieldsHintsAndCurrent);
+
+  var updatedHero = Hero(
+    id: hero.id,
+    name: update[0],
+    strength: int.tryParse(update[1]) ?? hero.strength,
+    gender: Gender.values.findMatch(update[2]) ?? hero.gender,
+    race: update[3],
+    alignment: Alignment.values.findMatch(update[4]) ?? hero.alignment
+  );
+
+  if (hero == updatedHero) {
+    print("No changes made");
+    return null;
+  }
+
+  if (promptForYesNo(
+        '''Save the following changes?${hero.sideBySide(updatedHero)}''',
+      ) ==
+      YesNo.no) {
+    return null;
+  }
+
+  return updatedHero;
+}
+
+List<String>? promptForValues(List<(String, String)> fieldsAndsHints) {
+  List<String> values = [];
+  for (var (field, hint) in fieldsAndsHints) {
+    print("Enter $field ($hint) or enter to abort:");
+    var input = (stdin.readLineSync() ?? "").trim();
+    if (input.isEmpty) {
+      return null;
+    }
+    values.add(input);
+  }
+  return values;
+}
+
+Hero? promptForNew() {
+  List<(String, String)> fieldsAndsHints = [];
+  for (int i = 1; i < Hero.fields.length; i++) {
+    var field = Hero.fields[i];
+    var hint = Hero.hints[i];
+    fieldsAndsHints.add((field, hint));
+  }
+
+  var values = promptForValues(fieldsAndsHints);
+  
+  if (values == null) {
+    return null;
+  }
+
+  var strength = int.tryParse(values[1]) ?? 0;
+  return Hero.newId(
+    values[0],
+    strength,
+    Gender.values.findMatch(values[2]) ?? Gender.unknown,
+    values[3],
+    Alignment.values.findMatch(values[4]) ?? Alignment.unknown,
+  );
+}
---
+++ v02/lib/persistence/hero_repository.dart
@@ -0,0 +1,133 @@
+import 'package:sqlite3/sqlite3.dart';
+import 'package:v02/jobs/job_queue.dart';
+import 'package:v02/models/hero.dart';
+import 'package:v02/utils/enum_parsing.dart';
+
+class HeroRepository {
+
+  HeroRepository.cache(this._db, this._cache);
+
+  factory HeroRepository(String path) {
+    final (db, heroes) = initDb(path);
+    return HeroRepository.cache(db, heroes);
+  }
+
+  static (Database, Map<String, Hero>) initDb(path) {
+    var db = sqlite3.open(path);
+    createTableIfNotExists(db);
+    return (db, readSnapshot(db));
+  }
+
+  static void createTableIfNotExists(Database db) {
+    db.execute('''
+CREATE TABLE IF NOT EXISTS heroes (
+  id TEXT PRIMARY KEY,
+	name TEXT NOT NULL,
+	strength INTEGER NOT NULL,
+	gender TEXT NOT NULL,
+	race TEXT NOT NULL,
+	alignment TEXT NULL
+)''');
+  }
+
+  static Map<String, Hero> readSnapshot(Database db) {
+    var snapshot = <String, Hero>{};
+
+    for (var row in db.select('SELECT * FROM heroes')) {
+      var hero = Hero(
+        id: row['id'] as String,
+        name: row['name'] as String,
+        strength: row['strength'] as int,
+        gender:
+            Gender.values.tryParse(row['gender'] as String) ?? Gender.unknown,
+        race: row['race'] as String,
+        alignment:
+            Alignment.values.tryParse(row['alignment'] as String) ??
+            Alignment.unknown,
+      );
+      snapshot[hero.id] = hero;
+    }
+    return snapshot;
+  }
+
+  void persist(Hero hero) {
+    _cache[hero.id] = hero;    
+    // Persist a copy to avoid race conditions (technically not needed for inserts but I want to keep the code nice and clean)
+    _jobQueue.enqueue(() => dbPersist(Hero.copy(hero)));
+  }
+
+  void dbPersist(Hero hero) {
+    _db.execute(
+      '''INSERT INTO heroes (id, name, strength, gender, race, alignment) VALUES (?, ?, ?, ?, ?, ?)
+ON CONFLICT (id) DO
+UPDATE
+SET name=excluded.name,
+    strength=excluded.strength,
+    gender=excluded.gender,
+    race=excluded.race,
+    alignment=excluded.alignment
+      ''',
+        [
+          hero.id,
+          hero.name,
+          hero.strength,
+          hero.gender.name,
+          hero.race,
+          hero.alignment.name
+        ]);
+  }
+
+  void delete(Hero hero) {
+    _cache.remove(hero.id);
+    _jobQueue.enqueue(() => dbDelete(hero));
+  }
+
+  void dbDelete(Hero hero) {
+    _db.execute('DELETE FROM heroes WHERE id = ?', [hero.id]);
+  }
+
+  void clean() {
+    _cache.clear();
+    _jobQueue.enqueue(() => dbClean());
+  }
+
+  void dbClean() {
+    _db.execute('DELETE FROM heroes');
+  }
+
+  List<Hero> query(String query) {
+    var lower = query.toLowerCase();
+    var result = _cache.values
+        .where((hero) => (hero.id.toLowerCase().contains(lower) ||
+            hero.name.toLowerCase().contains(lower) ||
+            hero.gender.toString().contains(lower) ||
+            hero.strength.toString().contains(lower) ||
+            hero.alignment.toString().contains(lower)))
+        .toList();
+
+    result.sort();    
+    return result;
+  }
+
+  // TODO: can I make a proper dispose pattern here?
+  Future<Null> dispose() async {
+    await _jobQueue.close();
+    await _jobQueue.join();
+    _db.dispose();
+    _cache.clear();
+  }
+
+  final JobQueue _jobQueue = JobQueue();
+  Map<String, Hero> _cache = {};
+
+  Database _db;
+
+  List<Hero> get heroes 
+  {
+    var snapshot = _cache.values.toList();
+    snapshot.sort();
+    return snapshot;
+  }
+
+  Map<String, Hero> get heroesById => Map.unmodifiable(_cache);
+}
